9607b2b7837a2eb1ee7a483b07b6bd60b40a76c5
atton <e115763@gmail.com>
2020-06-05 09:46:44 +0900

    Add --rm option to docker build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 
-docker build -t atton/latex-make    latex-make
-docker build -t atton/webpage-title webpage-title
+docker build --rm -t atton/latex-make    latex-make
+docker build --rm -t atton/webpage-title webpage-title

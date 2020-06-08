2e14df24a151e84dd06936e1f9254c5b64074355
atton <e115763@gmail.com>
2020-06-08 11:58:41 +0900

    Fix syntax warning

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ for name in `ls`; do
     tag="atton/$name"
     docker build $buildopt -t $tag $name
 
-    git diff-tree --no-commit-id --name-only -r HEAD | grep $name/ >& /dev/null
+    git diff-tree --no-commit-id --name-only -r HEAD | grep $name/ >/dev/null 2>&1
     if [ $? -eq 0 ]; then docker push $tag; fi
 done
 if [ -n "$1" ]; then docker logout; fi

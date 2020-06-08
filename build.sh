d71897a1299dbf5ef18ab92dc52cc2121228e614
atton <e115763@gmail.com>
2020-06-08 11:34:52 +0900

    Docker login/logout

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ cd `dirname $0`
 
 buildopt='--compress --pull'
 
+if [ -n "$1" ]; then docker login -u atton -p $1; fi
 for name in `ls`; do
     if [ ! -d $name ]; then continue; fi
     if [ ! -f $name/Dockerfile ]; then continue; fi
 
     docker build $buildopt -t atton/$name $name
 done
+if [ -n "$1" ]; then docker logout; fi
